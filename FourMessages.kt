2312192c2322874bc24d53b31211095002b17f0f
Jonathan Langdon <langdon.jonathan@gmail.com>
2026-04-30 21:58:29 -0400

    Four messages to console

---
+++ FourMessages.kt
@@ -0,0 +1,13 @@
+fun messages(): String {
+    val messagesArray = arrayOf(
+        "Use the val keyword when the value doesn't change.",
+        "Use the var keyword when the value can change.",
+        "When you define a function, you define the parameters that can be passed to it.",
+        "When you call a function, you pass arguments for the parameters."
+    )
+    return messagesArray.joinToString("\n")
+}
+
+fun main() {
+    println(messages())
+}
